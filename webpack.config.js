0ab11eb937b0ec2c8a6077fdea435be4ef859bad
Shreeraj Jadhav <shreeraj.jadhav@kitware.com>
2022-01-10 16:14:17 -0500

    COMP: Fix for glob error on windows
    Add use of path.posix.join to accomodate execution of
    "npm run start" command on windows platform.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ const entry = path.join(__dirname, 'src', 'index.js');
 const html = path.join(__dirname, 'src', 'index.html');
 const style = path.join(__dirname, 'src', 'style.css');
 const itkWebWorkers = path.join('itk', 'WebWorkers', 'ImageIO.worker.js');
-const itkImageIOs = path.join('itk', 'ImageIOs');
-const nodeModules = path.join(__dirname, 'node_modules');
+const itkImageIOs = path.posix.join('itk', 'ImageIOs');
+const posixDir = __dirname.replace(/\\/g, '/');
+const nodeModules = path.posix.join(posixDir, 'node_modules');
 const dist = path.join(__dirname, 'dist');
 
 module.exports = {
@@ -31,11 +32,11 @@ module.exports = {
           to: path.join(dist, itkWebWorkers),
         },
         {
-          from: path.join(nodeModules, itkImageIOs, '*DICOM*'),
+          from: path.posix.join(nodeModules, itkImageIOs, '*DICOM*'),
           to: path.join(dist, itkImageIOs, '[name][ext]'),
         },
         {
-          from: path.join(nodeModules, itkImageIOs, '*GDCM*'),
+          from: path.posix.join(nodeModules, itkImageIOs, '*GDCM*'),
           to: path.join(dist, itkImageIOs, '[name][ext]'),
         },
       ],
